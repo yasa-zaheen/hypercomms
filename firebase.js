7bdb38116988b72aa2c8c18ac56451a287a4d00a
Yasa Zaheen <yasazaheen@gmail.com>
2022-12-09 15:37:30 +0600

    Authentication

---
+++ firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBm57xS28rovozzKJ-seR86tdxMjyTeda8",
+  authDomain: "hypercomms-30a3a.firebaseapp.com",
+  projectId: "hypercomms-30a3a",
+  storageBucket: "hypercomms-30a3a.appspot.com",
+  messagingSenderId: "27952047671",
+  appId: "1:27952047671:web:16377ca69dc446f00601b2",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+export { auth };
--- pages/signIn.js
+++ pages/signIn.js
@@ -1,26 +1,54 @@
+import { useRouter } from "next/router";
 import Image from "next/image";
-import signInPic from "../public/signIn.jpg";
 
 import TextButton from "../components/TextButton";
 
+import { auth } from "../firebase";
+import {
+  signInWithRedirect,
+  GoogleAuthProvider,
+  onAuthStateChanged,
+} from "firebase/auth";
+
+import signInPic from "../public/signIn.jpg";
+
 function SignIn() {
-  return (
-    <div className="h-screen w-full space-x-4 overflow-hidden flex p-4 ">
-      <div className="w-1/3 bg-white rounded-xl overflow-hidden flex flex-col items-center justify-center">
-        <p className="text-2xl font-semibold">Welcome to hypercomms!</p>
-        <p className="text-sm font-light">The true successor to hyperchat 😉</p>
-        <TextButton text="Sign in with google" className={"m-4"} />
-      </div>
-      <div className="w-2/3 bg-blue-500 rounded-xl relative block overflow-hidden">
-        <Image
-          src={signInPic}
-          alt="Picture of the author"
-          fill
-          className="object-cover"
-        />
-      </div>
-    </div>
-  );
+  const router = useRouter();
+
+  const signInWithGoogle = () => {
+    const provider = new GoogleAuthProvider();
+    signInWithRedirect(auth, provider);
+  };
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      router.push("/");
+    } else {
+      return (
+        <div className="h-screen w-full space-x-4 overflow-hidden flex p-4 ">
+          <div className="w-1/3 bg-white rounded-xl overflow-hidden flex flex-col items-center justify-center">
+            <p className="text-2xl font-semibold">Welcome to hypercomms!</p>
+            <p className="text-sm font-light">
+              The true successor to hyperchat 😉
+            </p>
+            <TextButton
+              text="Sign in with google"
+              className={"m-4"}
+              onClick={signInWithGoogle}
+            />
+          </div>
+          <div className="w-2/3 rounded-xl relative block overflow-hidden">
+            <Image
+              src={signInPic}
+              alt="Sign in picture"
+              fill
+              className="object-cover"
+            />
+          </div>
+        </div>
+      );
+    }
+  });
 }
 
 export default SignIn;
