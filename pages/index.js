b88a70f08a0a1503fd3d89ec84f6c2a055aa5e94
Yasa Zaheen <yasazaheen@gmail.com>
2023-01-13 11:53:26 +0600

    Google adsense (2)

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,24 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import Landing from "../components/Landing";
 import ChatArea from "../components/ChatArea";
 import Loading from "../components/Loading";
+import Head from "next/head";
 
 export default function Home() {
   const [user, loading] = useAuthState(auth);
 
-  if (loading) return <Loading />;
-  else if (!user) return <Landing />;
-  else return <ChatArea user={user} />;
+  return (
+    <>
+      <Head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7299531318569051"
+          crossorigin="anonymous"
+        ></script>
+      </Head>
+      <body>
+        {loading ? <Loading /> : !user ? <Landing /> : <ChatArea user={user} />}
+        {}
+      </body>
+    </>
+  );
 }
