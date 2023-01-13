f97d3dd74a7630ce472564463a2789d1792709cc
Yasa Zaheen <yasazaheen@gmail.com>
2023-01-13 11:50:18 +0600

    Google adsense

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import '../styles/globals.css'
+import Script from "next/script";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        id="Adsense-id"
+        data-ad-client="ca-pub-7299531318569051"
+        async="true"
+        strategy="beforeInteractive"
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
